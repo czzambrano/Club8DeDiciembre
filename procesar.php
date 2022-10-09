9cc4c90e02f832711ec99aea434de7bc82ed63ea
CZZAMBRANO <c.zzambra@gmail.com>
2022-10-09 09:04:00 -0500

    corregido

--- procesar.php
+++ procesar.php
@@ -2,10 +2,10 @@
 $nombre = $_GET['nombre'];
 $correo = $_GET['mail'];
 $comentario = $_GET['comentario'];
-echo "<h2>informacion recibida desde php</h2>"
-echo "el nombre recibido es:" . $nombre . <br>;
-echo "el correo recibido es:" . $mail . <br>;
-echo "el comentario recibido es:" . $comentario . <br>;
+echo "<h2>informacion recibida desde php</h2>";
+echo "el nombre recibido es:" . $nombre ; 
+echo "el correo recibido es:" . $mail ;
+echo "el comentario recibido es:" . $comentario ;
 
 
 ?>               
