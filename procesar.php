5731843a9c31985ad9a265769cf7d8cd49464d6e
CZZAMBRANO <c.zzambra@gmail.com>
2022-10-09 19:28:00 -0500

    jj

--- procesar.php
+++ procesar.php
@@ -1,7 +1,7 @@
 <?php
-$nombre = $_POST['nombre'];
-$mail = $_POST['mail'];
-$comentario = $_POST['comentario'];
+$nombre = $_GET["nombre"];
+$mail = $_GET["mail"];
+$comentario = $_GET["comentario"];
 echo "<h2> informacion recibida desde php </h2>";
 echo  "el nombre recibido es:" . $nombre . "<br/>";
 echo  "el correo recibido es:" . $mail . "<br/>";
