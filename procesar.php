be0bf50c41d1b4ae09cc485689b8ac41e304abdd
CZZAMBRANO <c.zzambra@gmail.com>
2022-10-12 11:19:18 -0500

    con php

--- procesar.php
+++ procesar.php
@@ -1,11 +1,21 @@
 <?php
 $nombre = $_POST["nombre"];
-$mail = $_POST["mail"];
-$comentario = $_POST["comentario"];
+$email = $_POST["email"];
+$mensaje = $_POST["mensaje"];
 
-echo "<h2>INFORMACION DESDE PHP</h2>";
-echo "el nombre recibido es:" . $nombre . "<br/>";
-echo "el Mail recibido es:" . $mail . "<br/>";
-echo "el comentario recibido es:" . $comentario . "<br/>";
+$header = "From: " . $mail  . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
+$mensaje = "este mensaje fue enviado por:" . $nombre . ",\r\n";
+$mensaje = "Su email es :" . $email . "\r\n";
+$mensaje = "Mensaje: " . $_POST['mensaje'] . "\r\n";
+$mensaje = "Enviado el:" . date('dd/mm/yyyy', time());
+
+$para = 'c.zzambra@gmail.com';
+$asunto = 'asunto del mail recibido';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+header("Location: index.html");
 ?>
