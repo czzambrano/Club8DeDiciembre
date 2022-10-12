795462a48a3988fd57e6d7fd0ee1aafa11269c2c
CZZAMBRANO <c.zzambra@gmail.com>
2022-10-12 11:38:51 -0500

    ff

--- procesar.php
+++ procesar.php
@@ -1,9 +1,9 @@
 <?php
-$nombre = $_POST["nombre"];
-$email = $_POST["email"];
-$mensaje = $_POST["mensaje"];
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
 
-$header = "From: " . $mail  . " \r\n";
+$header = 'From: ' . $mail  . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version 1.0 \r\n";
 $header .= "Content-Type: text/plain";
