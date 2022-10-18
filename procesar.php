a546085c166719d9ff1606fc58dfbeed9c93ba1e
CZZAMBRANO <c.zzambra@gmail.com>
2022-10-18 18:13:32 -0500

    hh

---
+++ procesar.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(0);
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' . $mail  . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "este mensaje fue enviado por:" . $nombre . ",\r\n";
+$mensaje = "Su email es :" . $email . "\r\n";
+$mensaje = "Mensaje: " . $_POST['mensaje'] . "\r\n";
+$mensaje = "Enviado el:" . date('dd/mm/yyyy', time());
+
+$para = 'c.zzambra@gmail.com';
+$asunto = 'asunto del mail recibido';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+header("Location: index.html");
+echo"GRACIAS POR SU MENSAJE"
+?>
